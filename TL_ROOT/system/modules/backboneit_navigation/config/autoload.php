683313f902a2319177f57c0c1cf328b3fd4c81f0
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-03-21 18:10:57 +0100

    Added necessary autoload files

---
+++ TL_ROOT/system/modules/backboneit_navigation/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'AbstractModuleNavigation'         => 'system/modules/backboneit_navigation/AbstractModuleNavigation.php',
+	'ModuleNavigationChainPreorder'    => 'system/modules/backboneit_navigation/ModuleNavigationChainPreorder.php',
+	'ModuleNavigationMenu'             => 'system/modules/backboneit_navigation/ModuleNavigationMenu.php',
+	'NavigationDCA'                    => 'system/modules/backboneit_navigation/NavigationDCA.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'mod_backboneit_navigation_menu'   => 'system/modules/backboneit_navigation/templates',
+));
