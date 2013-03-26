64ed56c46ff827fdbc3fcca0bec931a2fcf0aab4
Oliver Hoff <oliver@hofff.com>
2013-03-26 17:25:35 +0100

    always create item array for root ids, for easier hook integration

--- TL_ROOT/system/modules/backboneit_navigation/ModuleNavigationMenu.php
+++ TL_ROOT/system/modules/backboneit_navigation/ModuleNavigationMenu.php
@@ -94,6 +94,10 @@ class ModuleNavigationMenu extends AbstractModuleNavigation {
 		$arrRootIDs = array_keys(array_flip($arrRootIDs));
 		$arrStop = (array) $arrStop;
 		
+		foreach($arrRootIDs as $intRootID) {
+			$this->arrItems[$intRootID] = (array) $this->arrItems[$intRootID];
+		}
+		
 		if($this->backboneit_navigation_includeStart) {
 			$arrConditions = array(
 			//	$this->getQueryPartHidden($this->backboneit_navigation_showHidden, $this->backboneit_navigation_isSitemap),
