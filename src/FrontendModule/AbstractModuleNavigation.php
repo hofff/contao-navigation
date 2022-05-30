c5fba898092a55bbae607aab38f7e466619550a0
David Molineus <david.molineus@netzmacht.de>
2022-05-30 15:48:18 +0200

    Rename hook bbit_navi_item to hofff_navigation_item

--- src/FrontendModule/AbstractModuleNavigation.php
+++ src/FrontendModule/AbstractModuleNavigation.php
@@ -158,7 +158,7 @@ abstract class AbstractModuleNavigation extends Module
 
         if (! $this->hofff_navigation_disableHooks) {
             $this->blnTreeHook = is_array($GLOBALS['TL_HOOKS']['hofff_navigation_tree']);
-            $this->blnItemHook = is_array($GLOBALS['TL_HOOKS']['bbit_navi_item']);
+            $this->blnItemHook = is_array($GLOBALS['TL_HOOKS']['hofff_navigation_item']);
         }
     }
 
@@ -766,7 +766,7 @@ abstract class AbstractModuleNavigation extends Module
             return;
         }
 
-        foreach ((array) $GLOBALS['TL_HOOKS']['bbit_navi_item'] as $arrCallback) {
+        foreach ((array) $GLOBALS['TL_HOOKS']['hofff_navigation_item'] as $arrCallback) {
             $this->import($arrCallback[0]);
             $this->{$arrCallback[0]}->{$arrCallback[1]}($this, $arrPage);
         }
