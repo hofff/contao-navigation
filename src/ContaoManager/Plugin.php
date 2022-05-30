3910fef5d45028e8bfb8891bec5781821998dc36
David Molineus <david.molineus@netzmacht.de>
2022-05-30 15:48:14 +0200

    Make it a bundle

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hofff\Contao\Navigation\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Hofff\Contao\Navigation\HofffContaoNavigationBundle;
+
+final class Plugin implements BundlePluginInterface
+{
+    /** {@inheritDoc} */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(HofffContaoNavigationBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['backboneit_navigation']),
+        ];
+    }
+}
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+use Hofff\Contao\Navigation\FrontendModule\ModuleNavigationMenu;
+
+$GLOBALS['FE_MOD']['navigationMenu']['backboneit_navigation_menu'] = ModuleNavigationMenu::class;
