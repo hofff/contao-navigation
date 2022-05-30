997e1eb5ce39a6e764936105c512af9bfc0ee4c3
David Molineus <david.molineus@netzmacht.de>
2022-05-30 15:48:21 +0200

    Rewrite the navigation module as fragment

--- src/FrontendModule/ModuleNavigationMenu.php
+++ src/FrontendModule/ModuleNavigationMenu.php
@@ -2,16 +2,19 @@
 
 namespace Hofff\Contao\Navigation\FrontendModule;
 
-use Contao\BackendTemplate;
-use Contao\Module;
-use Contao\System;
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
+use Contao\Environment;
+use Contao\Input;
+use Contao\ModuleModel;
+use Contao\Template;
 use Hofff\Contao\Navigation\Items\PageItemsLoader;
 use Hofff\Contao\Navigation\Renderer\NavigationRenderer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use function array_keys;
-use function strlen;
 
-use const TL_MODE;
 
 /**
  * Navigation modules
@@ -19,23 +22,23 @@ use const TL_MODE;
  * Navigation item array layout:
  * Before rendering:
  * id            => the ID of the current item (optional)
- * isInTrail        => whether this item is in the trail path
- * class        => CSS classes
- * title        => page name with Insert-Tags stripped and XML specialchars replaced by their entities
- * pageTitle    => page title with Insert-Tags stripped and XML specialchars replaced by their entities
- * link            => page name (with Insert-Tags and XML specialchars NOT replaced; as stored in the db)
- * href            => URL of target page
- * nofollow        => true, if nofollow should be set on rel attribute
+ * isInTrail     => whether this item is in the trail path
+ * class         => CSS classes
+ * title         => page name with Insert-Tags stripped and XML specialchars replaced by their entities
+ * pageTitle     => page title with Insert-Tags stripped and XML specialchars replaced by their entities
+ * link          => page name (with Insert-Tags and XML specialchars NOT replaced; as stored in the db)
+ * href          => URL of target page
+ * nofollow      => true, if nofollow should be set on rel attribute
  * target        => either ' onclick="window.open(this.href); return false;"' or empty string
- * description    => page description with line breaks (\r and \n) replaced by whitespaces
+ * description   => page description with line breaks (\r and \n) replaced by whitespaces
  *
  * Calculated while rendering:
- * subitems        => subnavigation as HTML string or empty string (rendered if subpages & items setup correctly)
- * isActive        => whether this item is the current active navigation item
+ * subitems      => subnavigation as HTML string or empty string (rendered if subpages & items setup correctly)
+ * isActive      => whether this item is the current active navigation item
  *
  * Following CSS classes are calculated while rendering: level_x, trail, sibling, submenu, first, last
  *
- * Additionally all page dataset values from the database are available unter their field name,
+ * Additionally, all page dataset values from the database are available unter their field name,
  * if the field name does not collide with the listed keys.
  *
  * For the collisions of the Contao core page dataset fields the following keys are available:
@@ -46,49 +49,33 @@ use const TL_MODE;
  * _description
  *
  * @author Oliver Hoff <oliver@hofff.com>
+ *
+ * @FrontendModule("hofff_navigation_menu", category="navigation")
  */
-final class ModuleNavigationMenu extends Module
+final class ModuleNavigationMenu extends AbstractFrontendModuleController
 {
-    protected $strTemplate = 'mod_hofff_navigation_menu';
-
-    protected string $strNavigation = '';
-
     private PageItemsLoader $loader;
 
     private NavigationRenderer $renderer;
 
-    public function __construct($objModule, $strColumn = 'main')
+    public function __construct(PageItemsLoader $loader, NavigationRenderer $renderer)
     {
-        parent::__construct($objModule, $strColumn);
-
-        $this->loader   = System::getContainer()->get(PageItemsLoader::class);
-        $this->renderer = System::getContainer()->get(NavigationRenderer::class);
-
-        if (TL_MODE === 'BE') {
-            return;
-        }
-
-        if (! strlen($this->navigationTpl)) {
-            $this->navigationTpl = 'nav_default';
-        }
+        $this->loader   = $loader;
+        $this->renderer = $renderer;
     }
 
-    public function generate(): string
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
     {
-        if (TL_MODE === 'BE') {
-            return $this->generateBE('NAVIGATION MENU');
-        }
-
-        $stopLevels = $this->getStopLevels();
-        $hardLevel  = $this->getHardLevel();
-        $activeId   = $this->hofff_navigation_isSitemap || $this->Input->get('articles')
+        $stopLevels = $this->getStopLevels($model);
+        $hardLevel  = $this->getHardLevel($model);
+        $activeId   = $model->hofff_navigation_isSitemap || Input::get('articles')
             ? null
             : (int) $GLOBALS['objPage']->id;
 
-        $items = $this->loader->load($this->objModel, $stopLevels, $hardLevel, $activeId);
+        $items = $this->loader->load($model, $stopLevels, $hardLevel, $activeId);
 
-        $this->strNavigation = $this->renderer->render(
-            $this->objModel,
+        $navigation = $this->renderer->render(
+            $model,
             $items,
             array_keys($items->roots),
             $stopLevels,
@@ -96,19 +83,29 @@ final class ModuleNavigationMenu extends Module
             $activeId
         );
 
-        return $this->strNavigation ? parent::generate() : '';
+        if ($navigation === '') {
+            return new Response();
+        }
+
+        $template->items       = $navigation;
+        $template->request     = Environment::get('indexFreeRequest');
+        $template->skipId      = 'skipNavigation' . $model->id;
+        $template->items       = $navigation;
+        $template->legacyClass = $model->hofff_navigation_addLegacyCss ? ' mod_navigation' : '';
+
+        return $template->getResponse();
     }
 
-    public function getStopLevels(): array
+    public function getStopLevels(ModuleModel $model): array
     {
-        if (! $this->hofff_navigation_defineStop) {
+        if (! $model->hofff_navigation_defineStop) {
             return [PHP_INT_MAX];
         }
 
         $minLevel  = -1;
         $stopLevel = [];
 
-        foreach (array_map('intval', explode(',', $this->hofff_navigation_stop)) as $level) {
+        foreach (array_map('intval', explode(',', $model->hofff_navigation_stop)) as $level) {
             if ($level > $minLevel) {
                 $stopLevel[] = $minLevel = $level;
             }
@@ -117,36 +114,8 @@ final class ModuleNavigationMenu extends Module
         return $stopLevel ?: [PHP_INT_MAX];
     }
 
-    public function getHardLevel()
+    public function getHardLevel(ModuleModel $model): int
     {
-        return $this->hofff_navigation_defineHard ? $this->hofff_navigation_hard : PHP_INT_MAX;
-    }
-
-    protected function compile(): void
-    {
-        $this->Template->request = $this->getIndexFreeRequest();
-        $this->Template->skipId  = 'skipNavigation' . $this->id;
-        $this->Template->items   = $this->strNavigation;
-        $this->hofff_navigation_addLegacyCss && $this->Template->legacyClass = ' mod_navigation';
-    }
-
-    /**
-     * A helper method to generate BE wildcard.
-     *
-     * @param string $strBEType (optional, defaults to "NAVIGATION") The type to be displayed in the wildcard
-     *
-     * @return string The wildcard HTML string
-     */
-    protected function generateBE($strBEType = 'NAVIGATION'): string
-    {
-        $objTemplate = new BackendTemplate('be_wildcard');
-
-        $objTemplate->wildcard = '### ' . $strBEType . ' ###';
-        $objTemplate->title    = $this->headline;
-        $objTemplate->id       = $this->id;
-        $objTemplate->link     = $this->name;
-        $objTemplate->href     = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
-
-        return $objTemplate->parse();
+        return $model->hofff_navigation_defineHard ? (int) $model->hofff_navigation_hard : PHP_INT_MAX;
     }
 }
--- src/Resources/contao/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Hofff\Contao\Navigation\FrontendModule\ModuleNavigationMenu;
-
-$GLOBALS['FE_MOD']['navigationMenu']['hofff_navigation_menu'] = ModuleNavigationMenu::class;
